328623deeaecc4e0c631d3f32ca7d23267f31cfe
Andrej Soukup <soukup.andrej@gmail.com>
2018-06-07 08:37:08 +0200

    event comments

--- frontend/views/site/event.php
+++ frontend/views/site/event.php
@@ -8,7 +8,8 @@
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
+use yii\widgets\LinkPager;
+use yii\widgets\Pjax;
 
 $this->title = $event['name'];
 ?>
@@ -17,7 +18,7 @@ $this->title = $event['name'];
 
     <p><?= $event['description'] ?></p>
     <!--	--><?php //var_dump($images) ?>
-    <?php if (!empty($images)):?>
+	<?php if (!empty($images)): ?>
 		<?php foreach ($images as $image): ?>
 			<?php
 			$i++;
@@ -30,9 +31,49 @@ $this->title = $event['name'];
                 <hr>
 			<?php endif; ?>
 		<?php endforeach; ?>
-    <?php endif; ?>
+	<?php endif; ?>
 
+    <div class="clearfix"></div>
+    <hr>
+    <div class="comment-form col-sm-6">
 
+		<?php $form = ActiveForm::begin(['action' => Url::to(['/comment/create', 'name' => $name])]); ?>
+
+		<?= $form->field($model, 'text')->textarea(['rows' => 2]) ?>
+		<?= $form->field($model, 'name_id')->hiddenInput(['value' => $event['id']])->label(false) ?>
+
+        <div class="form-group">
+			<?= Html::submitButton('Pridať komentár', ['class' => 'btn btn-success']) ?>
+        </div>
+
+		<?php ActiveForm::end(); ?>
+
+    </div>
+
+    <div class="clearfix"></div>
+
+	<?php Pjax::begin() ?>
+
+    <div class="all-comments">
+		<?php foreach ($comments as $comment): ?>
+            <div class="row">
+				<?= $comment['text'] ?> &nbsp;
+				<?php if (Yii::$app->user->can('superadmin')): ?>
+                    <p><a href="<?= Url::to(['/comment/delete', 'id' => $comment['id'], 'name' => $event['name']]) ?>">Vymazať</a>
+                    </p>
+                    <p><a href="<?= Url::to(['/comment/update', 'id' => $comment['id'], 'name' => $event['name']]) ?>">Upraviť</a>
+                    </p>
+				<?php endif; ?>
+            </div>
+		<?php endforeach; ?>
+    </div>
+
+	<?php
+	echo LinkPager::widget([
+		'pagination' => $pagination,
+	]);
+	Pjax::end();
+	?>
 </div>
 
 
